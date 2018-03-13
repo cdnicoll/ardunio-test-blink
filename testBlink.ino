745c8db1a628cda537a5b3b41f22f24528a1dd34
Cody Nicoll <cdnicoll@gmail.com>
2018-03-13 10:32:35 -0700

    first commit

---
+++ testBlink.ino
@@ -0,0 +1,14 @@
+int ledLight = 8;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledLight, OUTPUT);
+  Serial.begin(9600); // Starts the serial communication
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  digitalWrite(ledLight, true);
+  delay(100);
+  digitalWrite(ledLight, false);
+}
